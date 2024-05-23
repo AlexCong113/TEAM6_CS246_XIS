5bbb2fa5eb13858a2d89663858b97fe75b4eee56
vule556677 <161566212+vule556677@users.noreply.github.com>
2024-05-23 15:28:06 +0700

    Add files via upload

---
+++ CMU_CS246XIS_GROUP6/5.Refactoring/Exersice 2/studentt.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Packages;
+public class studentt {
+
+    private int id;
+    private String name;
+    private int classID;
+    private String birthday;
+    private String address;
+    private String phone;
+
+    public studentt(int id, String name, int classID, String birthday, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.classID = classID;
+        this.birthday = birthday;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getClassID() {
+        return classID;
+    }
+
+    public void setClassID(int classID) {
+        this.classID = classID;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
